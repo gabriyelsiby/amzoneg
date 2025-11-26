c9ed3bbf24ffd5e68e09822940e4a11df428e1dc
gabriyelsiby <gabriyel952003@gmail.com>
2025-11-26 15:12:19 +0530

    .

--- server/server.js
+++ server/server.js
@@ -44,5 +44,5 @@ const connectDB = async () => {
 };
 connectDB();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
